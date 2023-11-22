d9191b85d87f9540794ecf0246b8756cfcb32265
Theozmp <theozoumpourlis99@gmail.com>
2023-11-22 15:30:45 +0200

    ok

--- A1/A1.cpp
+++ A1/A1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <a.out.h>
 #include <cstring>
 using namespace std;
 
@@ -27,37 +26,48 @@ class Person{
 
 
 };
+//constuctor
 Person::Person():firstname(0), lastname(0),sdi(0), numoffirstanme(0),numoflastname(0),numofsdi(0){
     plithos++;
 }
-
+//copy constuctor
 Person::Person(const Person &p){
-    firstname= p.firstname;
-    lastname= p.lastname;
-    sdi=p.sdi;
-    numoffirstanme= p.numoffirstanme ;
-    numoflastname=p.numoflastname;
-    numofsdi=p.numofsdi;
-}
+    delete[]  firstname;
+    delete[] lastname;
+    delete[] sdi;
+    numoffirstanme = p.numoffirstanme;
+    numoflastname =p.numoflastname;
+    numofsdi = p.numofsdi;
 
+    firstname=new char[numoffirstanme+1];
+    lastname=new char[numoflastname+1];
+    sdi=new char[numofsdi+1];
+
+    strcpy(firstname, p.firstname);
+    strcpy(lastname, p.lastname);
+    strcpy(sdi, p.sdi);
+
+    plithos++;
+}
+//constructor
 Person::Person(char* f , char* l , char* s):numoffirstanme(strlen(f)), numoflastname(strlen(l)), numofsdi(strlen(s)){
-    firstname = new char[numoffirstanme+1];
-    lastname = new char[numoflastname +1];
+    firstname = new char[numoffirstanme + 1];
+    lastname = new char[numoflastname + 1];
     sdi = new char[numofsdi + 1];
     plithos++;
 }
-
+//deconstuctor
 Person::~Person(){
     delete[] firstname;
     delete[] lastname;
     delete[] sdi;
     plithos--;
 }
-
+//sunarthshsgia to plithos
  void Person::showplithos(){
     cout<<"number of person are "<< plithos << endl;
 }
-
+// sunarthshs gia  cin
 istream &operator >> (istream  &str , Person &obj ){
     cout<<"give firstname";
     str >> obj.firstname;
@@ -75,7 +85,7 @@ istream &operator >> (istream  &str , Person &obj ){
     str >> obj.numofsdi;
     return str;
 }
-
+//synarthshs gia cout
 ostream &operator << (ostream &str , Person &obj){
     str<< obj.firstname <<endl;
     str<< obj.lastname <<endl;
@@ -84,10 +94,24 @@ ostream &operator << (ostream &str , Person &obj){
 }
 
 
-int  Person::plithos=0; 
+int  Person::plithos=0; //arxikopoihshsh
 
 
 int main(){
+    Person person1("John", "Doe", "12345");
+    Person person2;
+
+    cout << "Person 1: " << person1 << endl;
+
+    cout << "Enter details for Person 2:" << endl;
+    cin >> person2;
+
+    cout << "Person 2: " << person2 << endl;
+
+    // Show the count of Person objects
+    person2.showplithos();
+
+    return 0;
 
 
  
